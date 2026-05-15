17fd7c9f02acf2a6f3a917de5012d08ec2021cc8
Sujal Modi <sujalmodi46@gmail.com>
2026-05-15 22:51:53 +0530

    first commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+    
+    @Test
+    public void testValidLogin() {
+        // Test steps for valid login
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername("admin@yourstore.com");
+        loginPage.enterPassword("admin");
+        loginPage.clickLoginButton();
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Just a moment...");
+    }
+}
